2fa11d090aca6045a0842ae717f573046770fe01
Hunter Caron <huntercaron@me.com>
2015-10-28 11:25:05 -0400

    dependancies and such

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.querySelector( "#toggle-box" ).addEventListener( "click", function() {
+  document.querySelector( "#nav-toggle" ).classList.toggle( "active" );
+  document.querySelector( "#nav-scaler" ).classList.toggle( "grow" );
+  document.querySelector( "#order-button" ).classList.toggle( "white" );
+  document.querySelector( "#order-text" ).classList.toggle( "green-text" );
+});
+
+console.log("hutn");
