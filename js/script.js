053c48cf80ad0235fc72f401d083c76ffc6bda53
Hunter Caron <huntercaron@me.com>
2015-10-29 23:18:42 -0400

    nav prototype and dynamic page load testing

--- js/script.js
+++ js/script.js
@@ -1,8 +1,39 @@
-document.querySelector( "#toggle-box" ).addEventListener( "click", function() {
-  document.querySelector( "#nav-toggle" ).classList.toggle( "active" );
-  document.querySelector( "#nav-scaler" ).classList.toggle( "grow" );
-  document.querySelector( "#order-button" ).classList.toggle( "white" );
-  document.querySelector( "#order-text" ).classList.toggle( "green-text" );
+$(document).ready(function()    {
+    $(function() {
+
+        if (Modernizr.history) {
+
+            _href = $(this).attr("href");
+
+            history.pushState(null, null, _href);
+
+            function loadContent(_href) {
+
+            }
+
+        } else {
+
+        }
+    });
+
+
+    var navOpen = false;
+
+    if (navOpen == false) {
+        $( "#nav-toggle" ).toggleClass( "active" );
+        $( "#nav-scaler" ).toggleClass( "grow" );
+        $( "#order-button" ).toggleClass( "white" );
+        $( "#order-text" ).toggleClass( "green-text" );
+        $( "nav" ).toggleClass( "hidden-nav" );
+    }
+
+    $( "#toggle-box" ).on("click", function() {
+      $( "#nav-toggle" ).toggleClass( "active" );
+      $( "#nav-scaler" ).toggleClass( "grow" );
+      $( "#order-button" ).toggleClass( "white" );
+      $( "#order-text" ).toggleClass( "green-text" );
+      $( "nav" ).toggleClass( "hidden-nav" );
+    });
 });
 
 console.log("hutn");
